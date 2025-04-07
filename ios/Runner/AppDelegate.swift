71f23912d9af640b5ab7ac1725997edd7aba3228
pavelkoifman <pavel4567@gmail.com>
2025-04-07 23:52:25 +0300

    added check for dart defines GOOGLE_MAPS_API_KEY

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,32 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCf5fdINazpeYF0lGTqjX4MnTZ4e7XhZtY")
+
+      if let defines = Bundle.main.object(forInfoDictionaryKey: "DART_DEFINES") as? String {
+                let decoded = defines
+                    .components(separatedBy: ",")
+                    .compactMap { $0.removingPercentEncoding } // decode percent encoding
+                    .compactMap { Data(base64Encoded: $0) }    // decode base64
+                    .compactMap { String(data: $0, encoding: .utf8) } // to string
+                    .map { $0.components(separatedBy: "=") }   // split to key-value
+                    .reduce(into: [String: String]()) { dict, pair in
+                        if pair.count == 2 {
+                            dict[pair[0]] = pair[1]
+                        }
+                    }
+                
+                if let apiKey = decoded["GOOGLE_MAPS_API_KEY"] {
+                    GMSServices.provideAPIKey(apiKey)
+                    print("✅ Google Maps API Key provided.")
+                } else {
+                    print("❌ mapsApiKey not found in DART_DEFINES.")
+                }
+            }
+
+      
+      
+ 
+    //GMSServices.provideAPIKey("AIzaSyCf5fdINazpeYF0lGTqjX4MnTZ4e7XhZtY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
